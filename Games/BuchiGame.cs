2034cd82360c2dc03a3f71b2aa7a026c19678f80
Alexander Weinert <weinert@react.uni-saarland.de>
2016-06-09 02:28:50 -0700

    Start implementation of Games

---
+++ Games/BuchiGame.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games
+{
+    public class Arena
+    {
+        private ISet<int> playerZeroNodes = new HashSet<int>();
+        private ISet<int> playerOneNodes = new HashSet<int>();
+        private ISet<Tuple<int,int>> edges = new HashSet<Tuple<int,int>>();
+
+        public void AddPlayerZeroNode(int id)
+        {
+            Debug.Assert(!playerZeroNodes.Contains(id));
+            Debug.Assert(!playerOneNodes.Contains(id));
+            playerZeroNodes.Add(id);
+        }
+
+        public void AddPlayerOneNode(int id)
+        {
+            Debug.Assert(!playerZeroNodes.Contains(id));
+            Debug.Assert(!playerOneNodes.Contains(id));
+            playerOneNodes.Add(id);
+        }
+
+        public void AddEdge(int from, int to)
+        {
+            Debug.Assert(playerZeroNodes.Contains(from) || playerOneNodes.Contains(from));
+            Debug.Assert(playerZeroNodes.Contains(to) || playerOneNodes.Contains(to));
+            var candEdge = new Tuple<int, int>(from, to);
+            Debug.Assert(!edges.Contains(candEdge));
+            edges.Add(candEdge);
+        }
+
+        public ISet<int> CPreZero(ISet<int> target)
+        {
+            var retVal = new HashSet<int>();
+            // Speaking in math: retVal += { cand \in V_0 | exists (v, v') \in E. v = cand && v' \in target }
+            retVal.UnionWith(from cand in playerZeroNodes where edges.Any(edge => edge.Item1 == cand && target.Contains(edge.Item2)) select cand);
+            // Speaking in math: retVal += { cand \in V_1 | forall (v, v') \in E. v = cand => v' \in target }
+            retVal.UnionWith(from cand in playerOneNodes where edges.All(edge => edge.Item1 != cand || target.Contains(edge.Item2)) select cand);
+            return retVal;
+        }
+
+        public ISet<int> CPreOne(ISet<int> target)
+        {
+            var retVal = new HashSet<int>();
+            // Speaking in math: retVal += { cand \in V_0 | forall (v, v') \in E. v = cand => v' \in target }
+            retVal.UnionWith(from cand in playerZeroNodes where edges.All(edge => edge.Item1 != cand || target.Contains(edge.Item2)) select cand);
+            // Speaking in math: retVal += { cand \in V_1 | exists (v, v') \in E. v = cand && v' \in target }
+            retVal.UnionWith(from cand in playerOneNodes where edges.Any(edge => edge.Item1 == cand && target.Contains(edge.Item2)) select cand);
+            return retVal;
+        }
+
+        public ISet<int> CPre(int player, ISet<int> target)
+        {
+            return player == 0 ? CPreZero(target) : CPreOne(target);
+        }
+    }
+
+    public class BuchiGame
+    {
+    }
+}
---
+++ GamesTests/ArenaTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Games;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games.Tests
+{
+    [TestClass()]
+    public class ArenaTests
+    {
+        [TestMethod()]
+        public void CPreZeroTest()
+        {
+            var arena = new Arena();
+            arena.AddPlayerZeroNode(0);
+            arena.AddPlayerOneNode(1);
+            arena.AddPlayerZeroNode(2);
+            arena.AddEdge(0, 1);
+            arena.AddEdge(1, 2);
+            arena.AddEdge(1, 1);
+            arena.AddEdge(2, 0);
+
+            var zeroSet = new HashSet<int>();
+            zeroSet.Add(0);
+
+            var twoSet = new HashSet<int>();
+            twoSet.Add(2);
+
+            var twoZeroSet = new HashSet<int>();
+            twoZeroSet.Add(0);
+            twoZeroSet.Add(2);
+
+            Assert.IsTrue(arena.CPreZero(twoSet).Count == 0);
+            Assert.IsTrue(arena.CPreZero(twoZeroSet).Count == 1 && arena.CPreZero(twoZeroSet).Contains(2));
+        }
+
+        [TestMethod()]
+        public void CPreOneTest()
+        {
+            var arena = new Arena();
+            arena.AddPlayerZeroNode(0);
+            arena.AddPlayerOneNode(1);
+            arena.AddPlayerZeroNode(2);
+            arena.AddEdge(0, 1);
+            arena.AddEdge(1, 2);
+            arena.AddEdge(1, 1);
+            arena.AddEdge(2, 0);
+
+            var zeroSet = new HashSet<int>();
+            zeroSet.Add(0);
+
+            var oneSet = new HashSet<int>();
+            oneSet.Add(1);
+
+            var twoSet = new HashSet<int>();
+            twoSet.Add(2);
+
+            Assert.IsTrue(arena.CPreOne(oneSet).Count == 2 && arena.CPreOne(oneSet).Contains(1) && arena.CPreOne(oneSet).Contains(0));
+            Assert.IsTrue(arena.CPreOne(twoSet).Count == 1 && arena.CPreOne(twoSet).Contains(1));
+        }
+    }
+}
